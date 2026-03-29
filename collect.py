8e2f2ac06e27b9ff746e34830c6702a716e3ac36
nario0715masa0619-create <nari.o.0715.masa.0619@gmail.com>
2026-03-29 22:04:21 +0900

    fix: collect.py を orchestrator の既存関数を使用するようにシンプル化

--- collect.py
+++ collect.py
@@ -12,7 +12,7 @@ import config
 from loguru import logger
 
 # ローカルモジュール
-from target_scraper import search_company_channels, get_channel_stats, filter_by_icp, ChannelData
+from orchestrator import _step1_scrape_targets, _step2_3_score_and_upsert, _step3_5_extract_emails, FlowResult
 from scorer import score_channels
 from crm_manager import upsert_lead
 from email_extractor import get_email_from_youtube_channel
@@ -100,6 +100,7 @@ def run_collect(keywords=None, dry_run=False):
 if __name__ == "__main__":
     logger.add("logs/collect.log", rotation="500 MB", retention="7 days")
     run_collect()
+
 
 
 
